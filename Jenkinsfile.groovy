769bfef321445af9c16c6f4ee0585755d22adb3f
Niraj Khanal <niraj.khanal@fractalindustries.com>
2017-06-28 17:39:23 -0400

    Adding files to test the versioning

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,42 @@
+#!/usr/bin/env groovy
+def scm_url = "git@bitbucket.org:fractalindustries/nginx-version-test"
+def scm_branch = "master"
+def docker_registry_url = "https://fractal-docker-fos-prod-bintray.io"
+def docker_image = "fractal-docker-fos-prod-bintray.io/nginx-version-test"
+
+def scm_credentials = "dcos-jenkins"
+def docker_registry_credentials = "dcos-jenkins-bintray"
+
+def jenkins_slave = "mesos"
+def marathon_url = "env.MARATHON_URL == null ? 'http://marathon.mesos:8080' : env.MARATHON_URL"
+
+def marathon_file_path = "marathon.json"
+def marathon_app_id = "nginx-version-test"
+
+
+node ( jenkins_slave ) {
+
+	stage ('Checkout') {
+		git url: scm_url, branch: scm_branch, credentialsId: scm_credentials
+	}
+
+	stage ('Docker') {
+		withDockerRegistry([credentialsId: docker_registry_credentials, url: docker_registry_url]) {
+			sh "git rev-parse --short HEAD > .git/commit_id"
+			def COMMIT_ID = readFile('.git/commmit_id').trim()
+
+		stage "Docker-Build"
+			docker.build(docker_image)
+	
+		stage "Docker-Push"		
+			app.push "$COMMIT_ID"
+		}
+	}
+
+	stage ('Marathon-Deployment') {
+		sh "./marathon.sh -i=$COMMIT_ID"
+		sh "curl -X PUT ${marathon_url}/v2/apps/${marathon_app_id} -d @${marathon_file_path} -H 'Content-type: application/json"
+	}
+}
+
+
---
+++ marathon.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+for i in "$@"
+do
+  case $i in 
+    -i=*|--tag=*)
+        TAG="${i#*=}"
+        ;;
+        *)
+        ;;
+esac
+done
+
+cat> marathon.json <<EOF
+{
+  "id": "nginx-version-test",
+  "container": {
+    "type": "DOCKER",
+    "docker": {
+      "image": "fractal-docker-registry.bintray.io/nginx-version-test:${TAG}",
+      "network": "BRIDGE",
+      "portMappings": [
+        { "hostPort": 0, "containerPort": 80, "servicePort": 0, "protocol": "tcp"}
+      ]
+    }
+  },
+  "instances":1,
+  "cpus": 0.1,
+  "mem": 64,
+  "uris": [
+    "file:///mnt/jenkins/docker.tar.gz"
+  ],
+  "labels": {
+    "HAPROXY_GROUP":"external",
+    "HAPROXY_0_VHOST":"nginx-version-test.fos"
+  }
+}
+EOF
